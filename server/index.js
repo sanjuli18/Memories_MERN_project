87373cb1003db7b5b775e347b420d9821606ef33
sanjuli18 <enlightenedsanjuli@gmail.com>
2021-12-15 18:36:09 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ app.use('/posts' , postRoutes);
 app.get('/',(req,res) => {
     res.send('Hello to Memories API');
 })
-// const CONNECTION_URL = 'mongodb+srv://sanjuli_trivedi:UvARTZe9N9w32dW@cluster0.9dtm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://sanjuli_trivedi:UvARTZe9N9w32dW@cluster0.9dtm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true , useUnifiedTopology: true})
 .then (() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
