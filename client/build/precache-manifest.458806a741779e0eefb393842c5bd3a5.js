f37d8115898d8b1a188c1711d0fa25481801ce78
sanjuli18 <enlightenedsanjuli@gmail.com>
2021-12-15 20:53:23 +0530

    updated

---
+++ client/build/precache-manifest.458806a741779e0eefb393842c5bd3a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5816acdc771adb6efab469c2a529c024",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da9e911655be36b11ad",
+    "url": "/static/css/main.99a7a7b6.chunk.css"
+  },
+  {
+    "revision": "f7413b7d26b2b4b3e0b3",
+    "url": "/static/js/2.66ba34c8.chunk.js"
+  },
+  {
+    "revision": "702664421703a10b78a9c0d5af38339c",
+    "url": "/static/js/2.66ba34c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da9e911655be36b11ad",
+    "url": "/static/js/main.d16c67df.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "43af08a0dc1bed54a6a5bb88b127274b",
+    "url": "/static/media/memories.43af08a0.jpg"
+  }
+]);
