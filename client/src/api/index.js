fad9653176030a0aef8f7c87632e2b902a068ef5
sanjuli18 <enlightenedsanjuli@gmail.com>
2021-12-19 20:37:35 +0530

    user actions were created so that the user can perform any action only when logged in

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000'})
+const API = axios.create({baseURL: 'http://localhost:5000'});
 
 //const url = 'https://memoriesss-app.herokuapp.com/posts';
+
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+});
 
 export const fetchPosts = () => API.get('/posts');
 
