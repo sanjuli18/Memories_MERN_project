c115e35067bcabe666c614aef15526b9949d58fe
sanjuli18 <enlightenedsanjuli@gmail.com>
2021-12-23 01:28:12 +0530

    implemented  comments functionality frontend

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -6,6 +6,7 @@ import { useParams, useHistory } from 'react-router-dom';
 
 import { getPost, getPostsBySearch } from '../../actions/posts';
 import useStyles from './styles';
+import CommentSection from './CommentSection';
 
 const Post = () => {
   const { post, posts, isLoading } = useSelector((state) => state.posts);
@@ -51,6 +52,8 @@ const Post = () => {
           {/* <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
           <Divider style={{ margin: '20px 0' }} />
           <Typography variant="body1"><strong>Comments - coming soon!</strong></Typography> */}
+
+          <CommentSection post={post} />
           <Divider style={{ margin: '20px 0' }} />
         </div>
         <div className={classes.imageSection}>
