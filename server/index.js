16885fa5c110040c14b1f61131196305dd1631a3
sanjuli18 <enlightenedsanjuli@gmail.com>
2021-12-24 23:11:35 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -15,17 +15,17 @@ dotenv.config();
 app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use('/posts' , postRoutes);
 app.use('/user' , userRoutes);
 
 app.get('/',(req,res) => {
     res.send('Hello to Memories API');
 });
-const CONNECTION_URL = 'mongodb+srv://sanjuli_trivedi:UvARTZe9N9w32dW@cluster0.9dtm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://sanjuli_trivedi:UvARTZe9N9w32dW@cluster0.9dtm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true , useUnifiedTopology: true})
 .then (() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
