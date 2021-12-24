5482b7159d0d61eb4be7c6fd0eded00e0bd9a513
sanjuli18 <enlightenedsanjuli@gmail.com>
2021-12-24 18:05:14 +0530

    procfile issues

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'https://memories-livid.vercel.app/'});
+const API = axios.create({baseURL: 'http://localhost:5000'});
 
 //const url = 'https://memoriesss-app.herokuapp.com/posts';
 
